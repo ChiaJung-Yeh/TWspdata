321f466e982f0ea65c7316826c435eca6e690c1a
ChiaJung-Yeh <robert1328.tem06@nctu.edu.tw>
2021-09-08 15:36:52 +0800

    Initial commit

--- data-raw/data_prep.R
+++ data-raw/data_prep.R
@@ -1,3 +1,20 @@
-## code to prepare `DATASET` dataset goes here
+library(gutenbergr)
 
-usethis::use_data(DATASET, overwrite = TRUE)
+timemachine <- gutenberg_download(35)$text
+timemachine <- timemachine[9:length(timemachine)]
+
+
+hsinchu_bus_route=read.csv("C:/Users/ASUS/Desktop/R Transportation/R Github Project/Spatial-Analysis/data/csv_files/hsinchu_bus_route.csv")
+
+
+
+## Add data files to project
+usethis::use_data(hsinchu_bus_route, overwrite = TRUE)
+
+
+devtools::install_github("ChiaJung-Yeh/TWspdata")
+
+
+
+library(TWspdata)
+TWspdata::timemachine
